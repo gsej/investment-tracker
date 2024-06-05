71455501237b4a8037c7f5d95842fde1f38c3da3
Gareth Erskine-Jones <gsej@erskinejones.com>
2024-06-05 09:50:45 +0100

    use name 'alternate' rather than 'alternative' in exchange rate file

--- code/FileReaders/ExchangeRates/ExchangeRate.cs
+++ code/FileReaders/ExchangeRates/ExchangeRate.cs
@@ -7,7 +7,7 @@ public class ExchangeRate
     [JsonPropertyName("base_currency")] 
     public string BaseCurrency { get; init; }
     
-    [JsonPropertyName("alternative_currency")]
+    [JsonPropertyName("alternate_currency")]
     public string AlternateCurrency { get; init; }
     
     public string Date { get; init; }
