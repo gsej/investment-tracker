ae67d51afd3e5fd13cd829a7b6c3318bbcab156b
Gareth Erskine-Jones <gsej@erskinejones.com>
2024-06-26 21:05:46 +0100

    fix resource group name

--- infrastructure/log-analytics.tf
+++ infrastructure/log-analytics.tf
@@ -1,6 +1,6 @@
 resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
     name                = "${var.service_name}-log-analytics"
     location            = var.location
-	resource_group_name = azurerm_resource_group.group.location
+	resource_group_name = azurerm_resource_group.group.name
     retention_in_days   = 30
 }
