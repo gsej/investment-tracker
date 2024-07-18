f70db4f5db026a453f0025e94af1bf87b3d7964a
Gareth Erskine-Jones <gsej@erskinejones.com>
2024-07-18 11:42:30 +0100

    sql connection string settings.

--- infrastructure/appservice.tf
+++ infrastructure/appservice.tf
@@ -14,5 +14,6 @@ resource "azurerm_linux_web_app" linuxapp {
 
     app_settings = {
         APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.ai.instrumentation_key
-    }   
+        SqlConnectionString = "Server=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.database.name};Persist Security Info=False;User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+}
 }
