6a7864b2eabea1403674fd63dd235f9ed543fff2
Gareth Erskine-Jones <gsej@erskinejones.com>
2025-08-08 09:11:19 +0100

    AccountSelector doesn't need button press in radio button mode

--- code/web-angular/src/app/account-selector/account-selector.component.ts
+++ code/web-angular/src/app/account-selector/account-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ChangeDetectionStrategy } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ChangeDetectionStrategy, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { Account } from '../models/account';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -8,10 +8,9 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
   imports: [ReactiveFormsModule],
   templateUrl: './account-selector.component.html',
   styleUrls: ['./account-selector.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
-
+  //changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AccountSelectorComponent implements OnChanges {
+export class AccountSelectorComponent implements OnChanges, OnInit, OnDestroy {
 
   @Input()
   public accounts: Account[] = []
@@ -22,12 +21,25 @@ export class AccountSelectorComponent implements OnChanges {
   accountSelectorForm: FormGroup;
   selectedAccounts: Set<string> = new Set<string>();
   pendingSelectedAccounts: Set<string> = new Set<string>();
+  isShiftPressed: boolean = false;
 
   constructor() {
-    this.accountSelectorForm = new FormGroup({});
+    // this.accountSelectorForm = new FormGroup({
+    //   selectedAccount: new FormControl('') // Initialize with an empty string for radio button mode
+    // });
+
+    this.accountSelectorForm = new FormGroup({
+      rad_selectedAccount: new FormControl('')
+    });
+    //   selectedAccount: new FormControl('') // Initialize with an empty string for radio button mode
+    // });
   }
 
   get hasChanges(): boolean {
+    if (!this.multiSelectMode) {
+      return false;
+    }
+
     if (this.selectedAccounts.size !== this.pendingSelectedAccounts.size) {
       return true;
     }
@@ -39,37 +51,128 @@ export class AccountSelectorComponent implements OnChanges {
     return false;
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if (this.accounts.length > 0) {
-      // Initialize form controls for each account
-      this.accounts.forEach(account => {
-        if (!this.accountSelectorForm.controls[account.accountCode]) {
-          this.accountSelectorForm.addControl(account.accountCode, new FormControl(false));
-        }
-      });
+  get multiSelectMode(): boolean {
+    return this.isShiftPressed || this.pendingSelectedAccounts.size > 1;
+  }
 
-      // If no accounts are selected and we have accounts, select the first one by default
-      if (this.selectedAccounts.size === 0) {
-        this.selectedAccounts.add(this.accounts[0].accountCode);
-        this.pendingSelectedAccounts.add(this.accounts[0].accountCode);
-        this.accountSelectorForm.controls[this.accounts[0].accountCode].setValue(true);
-        this.accountChanged.next(Array.from(this.selectedAccounts));
-      }
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Shift') {
+      this.isShiftPressed = true;
     }
   }
 
-  onAccountChanged(accountCode: string, event: Event) {
-    const checkbox = event.target as HTMLInputElement;
-
-    if (checkbox.checked) {
-      this.pendingSelectedAccounts.add(accountCode);
-    } else {
-      this.pendingSelectedAccounts.delete(accountCode);
+  @HostListener('window:keyup', ['$event'])
+  onKeyUp(event: KeyboardEvent) {
+    if (event.key === 'Shift') {
+      this.isShiftPressed = false;
     }
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    // Cleanup if needed
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+    if (!changes["accounts"]) {
+      return;
+    }
+
+    if (this.accounts.length > 0) {
+      this.accounts.forEach(account => {
+        const controlName = "chk_" + account.accountCode;
+        if (!this.accountSelectorForm.controls[controlName]) {
+          this.accountSelectorForm.addControl(controlName, new FormControl(false));
+        }
+      });
+
+      if (this.selectedAccounts.size === 0) {
+        this.selectedAccounts.add(this.accounts[0].accountCode);
+        this.pendingSelectedAccounts.add(this.accounts[0].accountCode);
+        this.updateFormControls();
+        this.accountChanged.next(Array.from(this.selectedAccounts));
+      }
+    }
+
+  }
+
+  private updateFormControls(): void {
+
+    this.accounts.forEach(account => {
+      const isSelected = this.pendingSelectedAccounts.has(account.accountCode);
+      this.accountSelectorForm.controls["chk_" + account.accountCode].setValue(isSelected);
+    });
+
+    const selectedAccount = Array.from(this.pendingSelectedAccounts)[0] || '';
+    this.accountSelectorForm.controls['rad_selectedAccount'].setValue(selectedAccount);
+  }
+
+  onAccountChanged(accountCode: string, event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    if (this.multiSelectMode) {
+      // Checkbox behavior - allow multiple selections
+      if (input.checked) {
+        this.pendingSelectedAccounts.add(accountCode);
+      } else {
+        this.pendingSelectedAccounts.delete(accountCode);
+
+        // if (this.pendingSelectedAccounts.size === 1) {
+        //   // If only one account is left selected, switch to radio button mode
+        //   this.accountSelectorForm.controls['rad_selectedAccount'].setValue(Array.from(this.pendingSelectedAccounts)[0], { emitEvent: false });
+        // }
+      }
+    } else {
+      // Radio button behavior - only one selection
+      if (input.checked) {
+        this.pendingSelectedAccounts.clear();
+        this.pendingSelectedAccounts.add(accountCode);
+
+       // this.accountSelectorForm.controls['rad_selectedAccount'].setValue(accountCode, { emitEvent: false });
+
+        // update the checkbox controls
+        // this.accounts.forEach(account => {
+        //   if (account.accountCode === accountCode) {
+        //     this.accountSelectorForm.controls["chk_" + account.accountCode].setValue(true);
+        //   }
+        //   else {
+        //     this.accountSelectorForm.controls["chk_" + account.accountCode].setValue(false);
+        //   }
+
+        // });
+
+      }
+
+      this.updateFormControls();
+
+
+    }
+
+    if (this.pendingSelectedAccounts.size === 1) {
+      // In radio button mode, apply changes immediately
+      this.selectedAccounts = new Set(this.pendingSelectedAccounts);
+      this.accountChanged.next(Array.from(this.selectedAccounts));
+    }
+
+    console.log('onAccountChanged - After changes:', {
+      pendingSelectedAccounts: JSON.stringify(Array.from(this.pendingSelectedAccounts)),
+      selectedAccounts: JSON.stringify(Array.from(this.selectedAccounts))
+    });
   }
 
   applyChanges() {
     this.selectedAccounts = new Set(this.pendingSelectedAccounts);
-    this.accountChanged.next(Array.from(this.selectedAccounts));
+    //this.updateFormControls();
+    //     this.accountChanged.next(Array.from(this.selectedAccounts));
+
+    console.log('applychanges - after update:', {
+      pendingSelectedAccounts: JSON.stringify(Array.from(this.pendingSelectedAccounts)),
+      selectedAccounts: JSON.stringify(Array.from(this.selectedAccounts))
+    });
   }
 }
