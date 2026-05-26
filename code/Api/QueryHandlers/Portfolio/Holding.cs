d36546e49f4e6fb5ffd37c12a8d578f8996bbdf3
Gareth Erskine-Jones <gsej@erskinejones.com>
2026-05-26 08:23:59 +0100

    remove old TODOs

--- code/Api/QueryHandlers/Portfolio/Holding.cs
+++ code/Api/QueryHandlers/Portfolio/Holding.cs
@@ -17,7 +17,7 @@ public class Holding
     public string StockDescription { get; init; }
     public string Allocation { get; init; }
     public decimal Quantity { get; init; }
-    public StockPriceResult StockPrice { get; set; } // TODO: make this readonly and add a method to set this and value
+    public StockPriceResult StockPrice { get; set; }
     public decimal ValueInGbp { get; set; }
     public string Comment { get; set; }
 }
